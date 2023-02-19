3d3fe4598d5975ba192c40952469934d9a1a6ddb
Raeein <raeeinbagheri80@gmail.com>
2023-02-19 00:05:12 -0500

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/raeeinbagheri/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/raeeinbagheri/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+# pyenv
+
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+# pyenv init - | source
